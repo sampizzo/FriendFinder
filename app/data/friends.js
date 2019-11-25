9097c653ff39befeae860eb6c3a43c9ac7778b69
SP <sampizzo@gmail.com>
2019-11-25 07:23:48 -0500

    added modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,7 @@
-var friends = [
-{
-    "name":"Fozzie Bear",
-    "photo":"https://vignette.wikia.nocookie.net/muppet/images/b/b5/Fozzie2.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
-];
+var friends = [{
+    "name": "Fozzie Bear",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/b/b5/Fozzie2.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+}];
 
 module.exports = friends;
