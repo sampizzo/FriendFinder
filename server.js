1e014ec7cec59c50834ec0e6bdfe062492afe586
SP <sampizzo@gmail.com>
2019-11-25 05:26:01 -0500

    fixed server.js router paths, changed submit btn, entered starter data for friends.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTER
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // LISTENER
 // Start the server
