87e761f3cbffa0c8975298dc3182d873fe3284b6
SP <sampizzo@gmail.com>
2019-11-25 13:01:49 -0500

    fixed apiRoutes.js, added Gonzo to friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,13 @@
 var friends = [{
     "name": "Fozzie Bear",
     "photo": "https://vignette.wikia.nocookie.net/muppet/images/b/b5/Fozzie2.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-}];
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+    "name": "Gonzo the Great",
+    "photo": "https://vignette.wikia.nocookie.net/muppet/images/e/eb/Gonzo2.jpg",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,13 +6,36 @@ var friends = require("../data/friends.js");
 // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 module.exports = function(app) {
-  
     app.get("/api/friends", function(req, res) {
-      res.json(friends);
-    });
-  
-    app.post("/api/friends", function(req, res) {
-   
-    });
-  };
-  
+        res.json(friends);
+      });
+    
+      app.post("/api/friends", function(req, res) {
+    
+        var difference = "";
+        var userMatchName = "";
+        var userMatchPhoto = "";
+        var userScore = req.body.scoresArray;
+    
+        for (var i = 0; i < friends.length; i++) {
+          var sum = 0;
+          for (var j = 0; j < userScore.length; j++) {
+            sum += Math.abs(userScore[j] - friends[i].scores[j]);
+          }
+          if (difference === "") {
+            userMatchName = friends[i].name;
+            userMatchPhoto = friends[i].photo;
+            difference = sum;
+          } else if (sum < difference) {
+            userMatchName = friends[i].name;
+            userMatchPhoto = friends[i].photo;
+            difference = sum;
+          }
+          console.log(difference, userMatchName);
+        }
+        // console.log(userMatchName)
+        friends.push(req.body);
+    
+        res.send({ name: userMatchName, photo: userMatchPhoto });
+      });
+    }
